139556f8577590dd7bdc27b690b76f1a63690943
NguyenPhuocHien <phuochien.mailbox@yahoo.com>
2017-06-01 10:24:59 +0700

    new commit

--- send.php
+++ send.php
@@ -3,10 +3,10 @@
 	use PhpAmqpLib\Connection\AMQPStreamConnection;
 	use PhpAmqpLib\Message\AMQPMessage;
 
-	if( $_GET["data"] )
+	if( $_POST["data"] )
   	{
-     		echo "You want to send message : ". $_GET['data']. "<br />";
-		$data=$_GET["data"];      
+     		echo "You want to send message : ". $_POST['data']. "<br />";
+		$data=$_POST["data"];      
 
   	}
 
@@ -14,16 +14,16 @@
 	$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 	$channel = $connection->channel();
 	$channel->queue_declare('hello', false, false, false, false);
-	#$msg = new AMQPMessage('Hello World!');
+	
 	
 
 	$msg = new AMQPMessage("$data");
 	
-	#$msg = $imes;
+	
 	
 	$channel->basic_publish($msg, '', 'hello');
 	
-	#echo " [x] Sent 'Hello World!'\n";
+	
 	echo $msg->body;
 	
 	$channel->close();
